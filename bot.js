600b705b5b2534a6a6f5c58eaf584e75adf37394
Danijel-Enoch <enochdanijel@gmail.com>
2023-07-13 22:55:45 +0100

    u

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const { authUser, addToken, getUserTokenAndBalances } = require("./src/util/api"
 const { Wallet, getGasPrice, getWalletAddress } = require("./src/util/blockchain")
 const { BSC_RPC_URL, ETH_RPC_URL, BSC_TESTNET, ETH_TESTNET } = require("./src/config");
 const { ethers, isAddress } = require("ethers");
-const bot = new Bot('6398162684:AAFSXX1Ik_e3L_IzuOp_bj4Se_CzUnBbGyM');
+const bot = new Bot('6363717865:AAG_4-2gyglT3irLDDuLR-BQpenBXRUA7i8');
 
 
 bot.use(session({ initial: () => ({ slippage: 0, chain: "", txWallet: "" }) }));
@@ -474,7 +474,7 @@ bot.command("start", async (ctx) => {
         console.log(bscWalletsBalances)
         //get bsc and eth Balance
 
-        const msg = `🤖Welcome to Earlyburdbot.\n⬩ BSC Gas ⛽️:  ${bscGasPrice} GWEI \n ⬩  ETH Gas ⛽️ :  ${ethGasPrice} GWEI \nSnipe & Swap with elite speed across multiple chains\n \n═══ Your Wallets ═══ \n ===BSC Balance=== \n Wallet 1 \n ${PublicKey[0]} \n Balance:${bscWalletsBalances[0]} \n Wallet 2 \n ${PublicKey[1]} \n Balance:${bscWalletsBalances[1]} \n Wallet 3 \n ${PublicKey[2]} \n Balance:${bscWalletsBalances[2]} \n \n =====ETH Balance==== \n Wallet 1 \n ${PublicKey[0]} \n Balance:${ethWalletsBalances[0]} \n Wallet 2 \n ${PublicKey[1]} \n Balance:${ethWalletsBalances[1]} \n Wallet 3 \n ${PublicKey[2]} \n Balance:${ethWalletsBalances[2]} `
+        const msg = `🤖Welcome to Lighteningfastbot 🤖\n⬩ BSC Gas ⛽️:  ${bscGasPrice} GWEI \n ⬩  ETH Gas ⛽️ :  ${ethGasPrice} GWEI \nSnipe & Swap with elite speed across multiple chains\n \n═══ Your Wallets ═══ \n ===BSC Balance=== \n Wallet 1 \n ${PublicKey[0]} \n Balance:${bscWalletsBalances[0]} \n Wallet 2 \n ${PublicKey[1]} \n Balance:${bscWalletsBalances[1]} \n Wallet 3 \n ${PublicKey[2]} \n Balance:${bscWalletsBalances[2]} \n \n =====ETH Balance==== \n Wallet 1 \n ${PublicKey[0]} \n Balance:${ethWalletsBalances[0]} \n Wallet 2 \n ${PublicKey[1]} \n Balance:${ethWalletsBalances[1]} \n Wallet 3 \n ${PublicKey[2]} \n Balance:${ethWalletsBalances[2]} `
 
         ctx.reply(msg, { reply_markup: menu })
 
