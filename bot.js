3d609a59a780905d222a031ad59ee3897e67fb52
Danijel-Enoch <enochdanijel@gmail.com>
2023-07-25 00:09:59 +0100

    updated css

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Telegraf, Markup } = require("telegraf");
-const { Bot, Context, session, InlineKeyboard, webhookCallback } = require("grammy");
+const { Bot, Context, session, InlineKeyboard, webhookCallback, GrammyError } = require("grammy");
 const { Menu } = require("@grammyjs/menu");
 const {
     conversations,
@@ -519,18 +519,18 @@ bot.command("updatePrivateKeys", async (ctx) => {
     const userData = await authUser(userId, ctx)
 
 })
-// bot.catch((err) => {
-//     const ctx = err.ctx;
-//     console.error(`Error while handling update ${ctx.update.update_id}:`);
-//     const e = err.error;
-//     if (e instanceof GrammyError) {
-//         console.error("Error in request:", e.description);
-//     } else if (e instanceof HttpError) {
-//         console.error("Could not contact Telegram:", e);
-//     } else {
-//         console.error("Unknown error:", e);
-//     }
-// });
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
+});
 
 function errorHandler(err) {
     console.error("Error in C!", err);
