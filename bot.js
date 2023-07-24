4beace03b767491421413266dea51f94441621a8
Danijel-Enoch <enochdanijel@gmail.com>
2023-07-25 00:06:04 +0100

    updated css

--- bot.js
+++ bot.js
@@ -519,18 +519,18 @@ bot.command("updatePrivateKeys", async (ctx) => {
     const userData = await authUser(userId, ctx)
 
 })
-bot.catch((err) => {
-    const ctx = err.ctx;
-    console.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
-    } else {
-        console.error("Unknown error:", e);
-    }
-});
+// bot.catch((err) => {
+//     const ctx = err.ctx;
+//     console.error(`Error while handling update ${ctx.update.update_id}:`);
+//     const e = err.error;
+//     if (e instanceof GrammyError) {
+//         console.error("Error in request:", e.description);
+//     } else if (e instanceof HttpError) {
+//         console.error("Could not contact Telegram:", e);
+//     } else {
+//         console.error("Unknown error:", e);
+//     }
+// });
 
 function errorHandler(err) {
     console.error("Error in C!", err);
